adeffa9d923320cd9e5fc1f28b1fcd5648ae2c12
Ian Ernest Voysey <iev@andrew.cmu.edu>
2016-01-26 16:32:57 -0500

    updating routes to do actual checking and redirecting, adding all the views

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+     // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    // 'url' : 'mongodb://<dbuser>:<dbpassword>@ds051615.mongolab.com:51615/heroku_pb14kkvr'
+    'url' : 'mongodb://heroku_pb14kkvr:rvkk41bp_ukoreh@ds051615.mongolab.com:51615/heroku_pb14kkvr'
+
 
 };
