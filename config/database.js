d4f59dc29f2c9ccd6d2b28f2ece06d4b41a36651
Ian Ernest Voysey <iev@andrew.cmu.edu>
2016-01-26 17:17:50 -0500

    new database user name and password

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@ module.exports = {
 
      // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     // 'url' : 'mongodb://<dbuser>:<dbpassword>@ds051615.mongolab.com:51615/heroku_pb14kkvr'
-    'url' : 'mongodb://heroku_pb14kkvr:rvkk41bp_ukoreh@ds051615.mongolab.com:51615/heroku_pb14kkvr'
+    // 'url' : 'mongodb://heroku_pb14kkvr:rvkk41bp_ukoreh@ds051615.mongolab.com:51615/heroku_pb14kkvr'
+    'url' : 'mongodb://heroapp:heroapp2@ds051615.mongolab.com:51615/heroku_pb14kkvr'
 
 
 };
