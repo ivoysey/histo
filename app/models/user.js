e653f91dc55c955b3eda34f927382e003e587d66
Ian Ernest Voysey <iev@andrew.cmu.edu>
2016-01-26 17:46:34 -0500

    ok whatever string; i'm sure d3 can figure it out

--- app/models/user.js
+++ app/models/user.js
@@ -8,7 +8,7 @@ var userSchema = mongoose.Schema({
     local            : {
         email        : String,
         password     : String,
-	age          : Integer
+	age          : String
     },
 });
 
