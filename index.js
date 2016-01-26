d8d096408e3370fa34c39dc2c76522f90dfe0753
Ian Ernest Voysey <iev@andrew.cmu.edu>
2016-01-26 16:14:24 -0500

    adding index stuff, passport still commented

--- index.js
+++ index.js
@@ -1,21 +1,70 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
+// this is all the stuff from the heroku set up guide
 
-app.set('port', (process.env.PORT || 5000));
+// var cool = require('cool-ascii-faces');
+// var express = require('express');
+// var app = express();
 
-app.use(express.static(__dirname + '/public'));
+// app.set('port', (process.env.PORT || 5000));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.use(express.static(__dirname + '/public'));
 
-app.get('/cool', function(request, response){ response.send(cool()); });
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+// app.get('/cool', function(request, response){ response.send(cool()); });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+
+// this is from the scotch.io guide.
+
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
